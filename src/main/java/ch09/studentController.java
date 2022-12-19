5c3776d492335c8f63ccf990988d7c096f5143ab
ksol46 <doomee880406@naver.com>
2022-12-19 18:09:58 +0900

    d

--- src/main/java/ch09/studentController.java
+++ src/main/java/ch09/studentController.java
@@ -34,16 +34,38 @@ public class studentController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8"); //한글데이터 깨짐 방지
 		String action = request.getParameter("action"); //쿼리스트링으로 줬음
-		//action을 실행하면 결국 insert를 가져온다. (?action=insert)
+		//action을 실행하면 insert혹은 list이다.
 		String view = "";
 		
-		view = insert(request,response); //request와 response 객체를 매개변수로 넘겨준다.
+		if (action == null) {
+			//리퀘스트를 또 한다.
+			getServletContext().getRequestDispatcher("/studentControl?action=list")//view : studentInfo.jsp 페이지명이 들어있음.
+			.forward(request,response);
+			//forward : 보안에도 좋고, request와 response를 넘겨준다.
+		} else {
+			switch(action) {
+			case "list": view = list(request,response); break;
+			case "insert": view = insert(request,response); break;
+			//request와 response 객체를 매개변수로 넘겨준다.
+			}
+			
+			//getServletContext():ServletContext를 얻어옴
+			//getRequestDispatcher(이동할 페이지):이동 할 페이지의 경로 지정
+			getServletContext().getRequestDispatcher("/ch09/" + view)//view : studentInfo.jsp 페이지명이 들어있음.
+			.forward(request,response);
+			//forward : 브라우저 내부에서 페이지를 이동함. 주소가 변하지 않는다.
+			
+		}
 		
-		//getServletContext():ServletContext를 얻어옴
-		//getRequestDispatcher(이동할 페이지):이동 할 페이지의 경로 지정
-		getServletContext().getRequestDispatcher("/ch09/" + view)//view : studentInfo.jsp 페이지명이 들어있음.
-		.forward(request,response);
-		//forward : 브라우저 내부에서 페이지를 이동함. 주소가 변하지 않는다.
+	}
+	
+	public String list(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("students",dao.getAll()); //request, response 할 때 값을 넘겨준다.
+								//key 키, object 객체 -> arraylist가 있을 것.
+		return "studentInfo.jsp";
+		//일단 학생의 정보럴 가져와서 setAttribute 해주고, return "studentInfo.jsp"; 해준다.
+		//request, response하는 과정에서 모든 정보를 가져올 수 있다.
+		//-> studentcontroller와 studentinfo를 가져오는것
 	}
     
 	//request 데이터 받아옴 -> DAO에 있는 insert 실행(DB에 insert가 됨) -> 페이지명(studentInfo.jsp) 리턴.
